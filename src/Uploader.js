6abf9174a5b57977ecc92e981d834682f4336b69
Arch0125 <dasarchisman25@gmail.com>
2022-09-03 16:17:22 +0530

    Update Interface

--- src/Uploader.js
+++ src/Uploader.js
@@ -8,6 +8,8 @@ const Uploader = () => {
 
     const [filePath, setFilePath] = React.useState(null);
     const[name,setName]=useState('');
+    const[url,setUrl]=useState(null);
+    const[ipfsimage,setIpfsimage]=useState(null);
     const[description,setDescription]=useState('');
     const NFT_STORAGE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGM0QThkRENiMGM1MjQwQTJDYjdBMzIwZGRGMDg5QUJhMERDNDVDZkYiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2MjE5NTgzMzEwMywibmFtZSI6InRwZyJ9.ncleHk0ziyvHC-8RSKo07nxdgJ9OidmguX_YzX-L0p4'
 
@@ -16,19 +18,25 @@ const Uploader = () => {
         console.log(filePath instanceof File)
         const nftstorage = new NFTStorage({ token: NFT_STORAGE_KEY })
 
-        var res = nftstorage.store({
+        var res = await nftstorage.store({
             image : filePath,
-            name,
-            description,
-        }).then(console.log)
+            name : name,
+            description : description,
+        })
 
-        
+        console.log(res)
+        setUrl(res.url)
+        setIpfsimage(res.ipnft)
     }
 
     return ( 
         <div>
             <input type={'file'} onChange={(e)=>setFilePath(e.target.files[0])} />
+            <input onChange={(e)=>setName(e.target.value)} placeholder="Enter NFT Name" />
+            <input onChange={(e)=>setDescription(e.target.value)} placeholder="Enter description" />
             <button onClick={()=>storeNFT(filePath,name,description)} >Upload to IPFS</button>
+            <label>Metadata : {url}</label>
+            <img src={`https://ipfs.io/ipfs/${ipfsimage}`} />
         </div>
      );
 }
